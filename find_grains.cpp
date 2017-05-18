b041ba8952f613180256f3f29e771d67efd73d3d
Jarin French <jarin.french@live.com>
2017-05-18 17:21:32 -0400

    Trying to make things more coherent.  Broke a couple of things, will need to fix them.

--- find_grains.cpp
+++ find_grains.cpp
@@ -51,13 +51,18 @@ bool pairSort(pair<int, double> &a, pair <int, double> &b)
   return (a.second < b.second);
 }
 
+bool pairGreater(pair<int, double> &a, pair <int, double> &b)
+{
+  return (a.second > b.second);
+}
+
 int main(int argc, char** argv)
 {
   string filename1, filename2, str; // filenames read from and written to, junk variable
   double xlow, xhigh, ylow, yhigh, zlow, zhigh, Lx, Ly, Lz; // bounds variables
   int N, n_type, n_atoms_read = 0; // number of atoms, atom types, number of atoms read
   vector <Atom> atoms; // all of the atoms from the file
-  vector <int> temp_atoms, counts; // Just the indices of the nn_atoms to atom i, counts for the symmetry parameter
+  vector <int> counts; // Just the indices of the nn_atoms to atom i, counts for the symmetry parameter
   vector <pair<int, vector<int> > > nn_atoms; // nearest neighbors
   vector <double> angles, symm; // a vector to hold the calculated angles, calculated symmetry parameters
   vector <pair <int, double> > symm_param, temp, unique_param; // vector holding the symmetry parameter for atom i
@@ -84,6 +89,7 @@ int main(int argc, char** argv)
     filename2 = filename1.substr(0,filename1.find(".dat")) + "_interface.dat";
   }
 
+  // Open up the files for reading and writing.
   ifstream fin(filename1.c_str());
   if (fin.fail())
   {
@@ -98,6 +104,7 @@ int main(int argc, char** argv)
     return 1;
   }
 
+  // Pull out the relevant information from the heading
   getline(fin, str); // Gets the comment line;
   fin >> N >> str; // Gets the number of atoms
   fin >> n_type >> str >> str; // gets the number of atom types
@@ -109,9 +116,15 @@ int main(int argc, char** argv)
   Ly = yhigh - ylow;
   Lz = zhigh - zlow;
 
-  cout << "Reading atoms...\n";
   while (fin >> id >> type >> charge >> x >> y >> z)
   {
+    if (type > n_type)
+    {
+      cout << "Error: unexpected atom type.\n"
+           << "n_types = " << n_type << " < this atom's type = " << type << endl;
+      return 2;
+    }
+    // Read the atoms line by line, and put them into a vector for analysis.
     Atom temp(id, type, charge, x, y, z);
     atoms.push_back(temp);
     ++n_atoms_read;
@@ -127,11 +140,9 @@ int main(int argc, char** argv)
   // Now that we have the atoms safely stored, we can process them.  Restarting
   // the counter to make sure we calculate the correct number of angles!
   n_atoms_read = 0;
-  cout << "Calculating nearest neighbors...\n";
+
   for (int i = 0; i < atoms.size(); ++i)
   {
-    temp_atoms.clear();
-
     x1 = atoms[i].getX();
     y1 = atoms[i].getY();
     z1 = atoms[i].getZ();
@@ -154,87 +165,53 @@ int main(int argc, char** argv)
           ryij = ryij - anInt(ryij / Ly) * Ly;
           rzij = rzij - anInt(rzij / Lz) * Lz;
 
+          // Calculate the distance
           drij_sq = (rxij * rxij) + (ryij * ryij) + (rzij * rzij);
 
+          // If we are within the cutoff distance, store it atoms for later.
           if (drij_sq < uu_cut_sq)
           {
-            // Place them in a vector for later analysis
-            // Note that this stores the atom ID, NOT the atom index!
-            temp_atoms.push_back(atoms[j].getId());
+            /* Now calculate the angle.  See the orientation parameter
+            ** calculated as in Bai et al. above, and in Zhang et al., Acta
+            ** Materialia, 53 (2005), 79-86
+            */
+            // We only use the x and y for the magnitude because we project
+            // the vector onto the x-y plane, effectively setting z = 0.
+            magnitude = sqrt(rxij * rxij + ryij * ryij);
+            theta = acos(rxij / magnitude); // gives the result in radians
+
+            // Store the angles for analysis after this loop
+            angles.push_back(theta);
           }
         }
       }
-    nn_atoms.push_back(make_pair(atoms[i].getId(), temp_atoms));
+
+      if (angles.size() == 0)
+      {
+        cout << "Error!  No angles calculated for atom " << atoms[i].getId() << endl;
+        return 4;
+      }
+
+      // Now we figure out the symmetry parameter of our atom
+      total = 0.0;
+      for (int j = 0; j < angles.size(); ++j)
+      {
+        sintheta = angles[j];
+        sintheta_sq = sintheta * sintheta;
+        total += (3 - 2 * sintheta_sq) * (3 - 2 * sintheta_sq) * sintheta_sq;
+      }
+      total /= angles.size();
+      symm_param.push_back(make_pair(atoms[i].getId(), total));
+      symm.push_back(anInt(total * 1000) / 1000.0);
     }
-    ++n_atoms_read;
   }
 
-  if (n_atoms_read != N)
-  {
-    cout << "Error: number of nearest neigbor sets does not match number of atoms in the simulation.\n"
-         << "N = " << N << " != n_atoms_read = " << n_atoms_read << endl;
-    return 3;
-  }
-
-  // Now calculate the angles.  See the orientation parameter calculated as in
-  // Bai et al. above, and in Zhang et al. Acta Materialia 53 (2005) 79-86
-  n_atoms_read = 0;
-  cout << "Calculating symmetry parameters...\n";
-
-  for (int i = 0; i < nn_atoms.size(); ++i)
-  {
-
-    // for each set, calculate the vector distance between the atom pairs
-    // The -1 is here because the IDs are (assumed to be) indexed from 1, not 0
-    // We normalize everything to a single x-y plane, so the z distances are not
-    // calculated.
-    x = atoms[(nn_atoms[i].first) - 1].getX();
-    y = atoms[(nn_atoms[i].first) - 1].getY();
-    // The distances are projected onto the x-y plane, so the z component is always 0
-
-    angles.clear(); // clear our angles vector for the next atom
-    for (int j = 0; j < nn_atoms[i].second.size(); ++j)
-    {
-      // Calculate the angle for this pair
-      rxij = atoms[(nn_atoms[i].second)[j] - 1].getX() - x;
-      ryij = atoms[(nn_atoms[i].second)[j] - 1].getY() - y;
-
-      // Apply PBCs
-      rxij = rxij - anInt(rxij / Lx) * Lx;
-      ryij = ryij - anInt(ryij / Ly) * Ly;
-
-      magnitude = sqrt(rxij * rxij + ryij * ryij);
-
-      theta = acos(rxij / magnitude); // gives the result in radians
-
-      angles.push_back(theta);
-      // Double check to see if we have already added in an angle for this atom
-      // Add it into the map if we don't already have it
-    }
-
-    if (angles.size() == 0)
-    {
-      cout << "Error!  No angles calculated for atom " << nn_atoms[i].first << endl;
-      return 4;
-    }
-
-    // Now we figure out the symmetry parameter of our atom
-    total = 0.0;
-    for (int j = 0; j < angles.size(); ++j)
-    {
-      sintheta = angles[j];
-      sintheta_sq = sintheta * sintheta;
-      total += (3 - 2 * sintheta_sq) * (3 - 2 * sintheta_sq) * sintheta_sq;
-    }
-    total /= angles.size();
-    symm_param.push_back(make_pair(nn_atoms[i].first, total));
-  }
-
-  temp = symm_param;
-  sort(symm_param.begin(), symm_param.end(), pairSort);
-  vector <pair <int, double> >::iterator it;
-  it = unique(symm_param.begin(), symm_param.end(), pairCmp);
-  symm_param.resize(distance(symm_param.begin(), it));
+  // Find the unique values of the symmetry parameter
+  temp = symm_param; // save the original data set
+  sort(symm_param.begin(), symm_param.end(), pairSort); // sort it
+  vector <pair <int, double> >::iterator it; // initialize an iterator
+  it = unique(symm_param.begin(), symm_param.end(), pairCmp); // determine where the unique values stop
+  symm_param.resize(distance(symm_param.begin(), it)); // resize the sorted vector
 
   for (int i = 0; i < symm_param.size(); ++i)
   {
@@ -242,39 +219,34 @@ int main(int argc, char** argv)
   }
   unique_param = symm_param;
   symm_param = temp;
-
-  for (int i = 0; i < symm_param.size(); ++i)
-  {
-    symm.push_back(anInt(symm_param[i].second * 1000) / 1000.0);
-  }
+  // FIXME: There shouldn't be this many unique symmetry parameters
+  cout << "There are " << unique_param.size() << " unique symmetry parameters.\n";
 
   for (int i = 0; i < unique_param.size(); ++i)
   {
     counts.push_back(count(symm.begin(), symm.end(), unique_param[i].second));
     counter += counts[i];
-    if (counts[i] > max1)
-    {
-      max2 = max1;
-      max1 = unique_param[i].second;
-
-      max2_index = max1_index;
-      max1_index = i;
-    }
-    else if (counts[i] > max2)
-    {
-      max2 = unique_param[i].second;
-      max2_index = i;
-    }
+    //cout << "unique_param[" << i << "].second = " << unique_param[i].second << " occurs " << counts[i] << " times.\n";
   }
 
-  cout << "The max number of counts occurs at " << max1_index << " with a value of " << max1 << " occurring " << counts[max1_index] << " times\n"
-       << "The second max number of counts occurs at " << max2_index << " with a value of " << max2 << " occurring " << counts[max2_index] << " times\n";
-
-  if (counter != nn_atoms.size())
+  if (counter != symm_param.size())
   {
-    cout << "Error: total number of counts is not equal to number of atoms calculated!\n"
-         << "counter = " << counter << " != nn_atoms.size() = " << nn_atoms.size() << endl;
+    cout << "Error in counting the elements\n";
+    return 6;
   }
+
+  max1 = *max_element(counts.begin(), counts.end());
+  max1_index = distance(counts.begin(), max_element(counts.begin(), counts.end()));
+
+  vector <int> count_temp = counts;
+  nth_element(counts.begin(), counts.begin()+1, counts.end(), greater<int>());
+  vector <int> sorted_counts = counts;
+  counts = count_temp;
+  max2 = *find(counts.begin(), counts.end(), sorted_counts[1]);
+  max2_index = distance(counts.begin(), find(counts.begin(), counts.end(), sorted_counts[1]));
+
+  cout << "The max number of counts occurs at " << max1_index << " with a value of " << unique_param[max1_index].second << " occurring " << counts[max1_index] << " times\n"
+       << "The second max number of counts occurs at " << max2_index << " with a value of " << unique_param[max2_index].second << " occurring " << counts[max2_index] << " times\n";
 
   /*// Make sure we write the entire set of atoms
   n_atoms_read = 0;
