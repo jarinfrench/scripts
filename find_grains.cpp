12e30f44fe7f20b0f338fdfca2b0c5f47e3e8b3a
Jarin French <jarin.french@live.com>
2017-06-19 10:30:52 -0400

    Added some more comments to clarify details of the code.

--- find_grains.cpp
+++ find_grains.cpp
@@ -15,7 +15,7 @@ using namespace std;
 // Cutoff distances
 #define UU_CUT 0.866 // from Bai et al. Acta Materialia 85 (2015) 95-106: 0.866 * a0
 #define a0 5.6 // typically 5.453
-#define IDEAL 1.0 / 6.0
+#define IDEAL 1.0 // 6.0 // This is dependent on the coefficients chosen for coeffs!
 
 // Calculate the rounded value of x
 double anInt(double x)
@@ -29,29 +29,31 @@ double anInt(double x)
 
 int main(int argc, char** argv)
 {
-  double coeffs [2] = {2,2};
+  double coeffs [2] = {3,2}; // Coefficients of the symmetry parameter
 
   string filename1, filename2, str; // filenames read from and written to, junk variable
   double xlow, xhigh, ylow, yhigh, zlow, zhigh, Lx, Ly, Lz; // bounds variables
   int N, n_type, n_atoms_read = 0; // number of atoms, atom types, number of atoms read
   vector <Atom> atoms; // all of the atoms from the file
-  vector <int> counts; // Counts for the symmetry parameter
   vector <double> symm; // a vector to hold the calculated symmetry parameters
-  int atom_id, type; // id and type number of atom
-  double charge, x, y, z; // charge and position of atom
-  double rxij, ryij, rzij, drij_sq;
-  double uu_cut_sq = UU_CUT * UU_CUT;
-  double theta, sintheta_sq, total = 0.0, xtemp, ytemp, sintheta, costheta, cutoff;
-  bool dump = true;
+  int atom_id, type; // id and type number of atom; used to read in the data
+  double charge, x, y, z; // charge and position of atom, used to read in data
+  double rxij, ryij, rzij, drij_sq; // positions and distance squared
+  double uu_cut_sq = UU_CUT * UU_CUT; // cutoff distance
+  double theta, sintheta_sq, total = 0.0; // misorientation angle, sin^2 of the angle, symmetry parameter (it's a sum, so starts at 0)
+  double xtemp, ytemp, sintheta, costheta, cutoff; // rotated x position, y position, sin theta, cos theta, cutoff for which grain an atom belongs to.
+  bool dump; // boolean value to determine if the read file is a LAMMPS dump file or not.
+  unsigned int n_grain_1, n_grain_2; // counter for number of atoms in each grain
 
   // Variables used for the cell-linked list
-  int n_atoms_per_cell;
+  int n_atoms_per_cell; // self-explanatory
   vector <vector <int> > iatom; // Cell-linked list
   vector <vector <vector <int> > > icell; // cell index
   vector <vector <vector <vector <int> > > > pcell; // atom index in each cell
   int ncellx, ncelly, ncellz, idx, idy, idz; // Number of sub cells in each direction, cell number in each direction, atoms in cell i
   double lcellx, lcelly, lcellz; // length of sub cells in each direction
 
+  // Parse command line input. Prompt for what we need.
   if (argc == 2)
   {
     filename1 = argv[1];
@@ -71,12 +73,12 @@ int main(int argc, char** argv)
     cin  >> theta;
   }
   filename2 = filename1.substr(0,filename1.find(".dat")) + "_interface.dat";
-  sintheta = sin(theta * PI / 180.0);
+  sintheta = sin(theta * PI / 180.0); // best to calculate this once
   costheta = cos(theta * PI / 180.0);
 
   // Calculate the cutoff value for grain identification
-  vector <double> xx (12,0.0);
-  vector <double> yy (12,0.0);
+  vector <double> xx (12,0.0); // x positions in terms of a0 for nearest neighbors
+  vector <double> yy (12,0.0); // y positions in terms of a0 for nearest neighbors
   xx[0] = 0.5; xx[1] = 0.5; xx[2] = -0.5; xx[3] = -0.5; xx[4] = 0.5; xx[5] = 0.5; xx[6] = -0.5; xx[7] = -0.5;
   yy[0] = 0.5; yy[1] = -0.5; yy[2] = 0.5; yy[3] = -0.5; yy[8] = 0.5; yy[9] = 0.5; yy[10] = -0.5; yy[11] = -0.5;
 
@@ -84,11 +86,24 @@ int main(int argc, char** argv)
   {
     xtemp = costheta * xx[i] - sintheta * yy[i];
     ytemp = sintheta * xx[i] + costheta * yy[i];
+    // Uses the idea that sin^2 = 1-cos^2
+    // cos = A.B / (|A||B|), and since B is (1,0,0), this simplifies to
+    // cos = A_x / |A|, meaning that cos^2 = A_x^2/(A_x^2+A_y^2)
     sintheta_sq = 1 - ((xtemp * xtemp) / (xtemp * xtemp + ytemp * ytemp));
+    // Symmetry parameter as defined by Zhang. Coefficients are changed to get
+    // better resolution between grains.
     total += (coeffs[0] - coeffs[1] * sintheta_sq) * (coeffs[0] - coeffs[1] * sintheta_sq) * sintheta_sq;
   }
   total /= xx.size();
+  // Cutoff is the midpoint between the ideal value and the rotated ideal value.
+  // Note that this assumes that the outside grain is oriented with it's x axis
+  // aligned with the x axis of the lab frame.
+  // FIXME: this needs to be generalized for outside grains that aren't aligned
   cutoff = (IDEAL + total) / 2.0;
+
+  // Output the values for documentation.
+  cout << "The value for grain 1 is " << IDEAL
+       << " and the value for grain 2 is " << total << endl;
 
   // Open up the files for reading and writing.
   ifstream fin(filename1.c_str());
@@ -106,18 +121,19 @@ int main(int argc, char** argv)
   }
 
   // Pull out the relevant information from the heading
-    if (filename1.find("dump") == string::npos)
+  // Determine if the file is a dump file or an input file
+    if (filename1.find("dump") == string::npos) // if "dump" is not in the filename
   {
-    dump = false;
+    dump = false; // it isn't a dump file, assume the file is an input file.
   }
   else
   {
-    dump = true;
+    dump = true; // if it is in the filename, assume that it's a LAMMPS dump file
   }
-  // This is for a LAMMPS dump file
 
   if (dump)
   {
+    // This is for a LAMMPS dump file
     getline(fin, str); // Gets ITEM: TIMESTEP
     getline(fin, str); // Gets the timestep number
     getline(fin, str); // Gets ITEM: NUMBER OF ATOMS
@@ -153,6 +169,8 @@ int main(int argc, char** argv)
   Ly = yhigh - ylow;
   Lz = zhigh - zlow;
 
+  // Preallocate the information (saves time, and allows the atoms to be written
+  // in order)
   atoms.resize(N, Atom());
   // Read the data
   while (fin >> atom_id >> type >> charge >> x >> y >> z)
@@ -169,6 +187,8 @@ int main(int argc, char** argv)
     x = x / a0 - xlow;
     y = y / a0 - ylow;
     z = z / a0 - zlow;
+    // We make the atom id match (almost) the index.  There is a difference of 1
+    // because C++ indexes from 0.
     atoms[atom_id - 1] = Atom(atom_id, type, charge, x, y, z);
     ++n_atoms_read;
   }
@@ -183,68 +203,72 @@ int main(int argc, char** argv)
   }
 
   // Generate the cell-linked list for fast calculations
+  // First generate the number of cells in each direction (minimum is 1)
   ncellx = (int)(Lx / UU_CUT) + 1;
   ncelly = (int)(Ly / UU_CUT) + 1;
   ncellz = (int)(Lz / UU_CUT) + 1;
-  lcellx = Lx / ncellx;
+  lcellx = Lx / ncellx; // Length of the cells in each direction
   lcelly = Ly / ncelly;
   lcellz = Lz / ncellz;
 
+  // Number of atoms per cell based on cell size, with a minimum allowed of 200
   n_atoms_per_cell = max((int)(N / (double)(3 * ncellx * ncelly * ncellz)), 200);
 
-  // resizes the vectors to be the correct length.
+  // resizes the vectors to be the correct length. Saves on time.
   // Defaults all values to 0
-  icell.resize(ncellx, vector <vector <int> >
-              (ncelly, vector <int>
-              (ncellz, 0)));
-  pcell.resize(ncellx, vector <vector <vector <int> > >
-              (ncelly, vector <vector <int> >
-              (ncellz, vector <int>
-              (n_atoms_per_cell, 0))));
-  iatom.resize(n_atoms_per_cell, vector <int> (N,0));
+  icell.resize(ncellx, vector <vector <int> > // x dimension
+              (ncelly, vector <int> // y dimension
+              (ncellz, 0))); // z dimension
+  pcell.resize(ncellx, vector <vector <vector <int> > > // x dimension
+              (ncelly, vector <vector <int> > // y dimension
+              (ncellz, vector <int> // z dimension
+              (n_atoms_per_cell, 0)))); // atom number in cell.
+  iatom.resize(n_atoms_per_cell, vector <int> (N,0)); // the actual list.
 
-  // This creates the cell-linked list
   /****************************************************************************/
-  /*************************CREATE CELL-LINKED LIST****************************/
+  /**************************CREATE CELL-LINKED LIST***************************/
   /****************************************************************************/
-  for (unsigned int i = 0; i < atoms.size(); ++i)
+  for (unsigned int i = 0; i < atoms.size(); ++i) // Look at each atom
   {
     if (atoms[i].getType() != 1) continue; // Only want U atoms
     // Assign this atom to a cell
     // Rounds towards 0 with a type cast
-    idx = (int)(atoms[i].getX() / lcellx);
-    idy = (int)(atoms[i].getY() / lcelly);
-    idz = (int)(atoms[i].getZ() / lcellz);
+    idx = (int)(atoms[i].getX() / lcellx); // assign the x cell
+    idy = (int)(atoms[i].getY() / lcelly); // assign the y cell
+    idz = (int)(atoms[i].getZ() / lcellz); // assign the z cell
     // Check if we went out of bounds
-    // C++ indexes from 0, so we have to subtract 1 from the maximum value to stay within our memory bounds
+    // C++ indexes from 0, so we have to subtract 1 from the maximum value to
+    // stay within our memory bounds
     if (idx >= ncellx) idx = ncellx - 1;
     if (idy >= ncelly) idy = ncelly - 1;
     if (idz >= ncellz) idz = ncellz - 1;
 
     ++icell[idx][idy][idz]; // increase the number of atoms in this cell
+    // assign the atom number to this index.
     pcell[idx][idy][idz][icell[idx][idy][idz] - 1] = i;
   }
 
-  for (int i = 0; i < ncellx; ++i)
+  for (int i = 0; i < ncellx; ++i) // For each x cell
   {
-    for (int j = 0; j < ncelly; ++j)
+    for (int j = 0; j < ncelly; ++j) // For each y cell
     {
-      for (int k = 0; k < ncellz; ++k)
+      for (int k = 0; k < ncellz; ++k) // For each z cell
       {
-        for (int l = 0; l < icell[i][j][k]; ++l)
+        for (int l = 0; l < icell[i][j][k]; ++l) // For each atom in this cell
         {
-          int id = pcell[i][j][k][l];
+          int id = pcell[i][j][k][l]; // store this atom id
           // Now we check each sub cell around the current one
-          for (int ii = -1; ii < 2; ++ii)
+          for (int ii = -1; ii < 2; ++ii) // allowed values: -1, 0, and 1
           {
             for (int jj = -1; jj < 2; ++jj)
             {
               for (int kk = -1; kk < 2; ++kk)
               {
-                int ia = i + ii;
+                int ia = i + ii; // min value: -1.  Max value: number of cells in dimension
                 int ja = j + jj;
                 int ka = k + kk;
                 // Check to make sure we are still in bounds
+                // C++ indexes from 0, so we accomodate.
                 if (ia >= ncellx) ia = 0;
                 if (ja >= ncelly) ja = 0;
                 if (ka >= ncellz) ka = 0;
@@ -252,6 +276,7 @@ int main(int argc, char** argv)
                 if (ja < 0) ja = ncelly - 1;
                 if (ka < 0) ka = ncellz - 1;
 
+                // Now check each atom in this cell
                 for (int m = 0; m < icell[ia][ja][ka]; ++m)
                 {
                   int jd = pcell[ia][ja][ka][m];
@@ -279,7 +304,7 @@ int main(int argc, char** argv)
                     continue; // move to the next atom if we're too far away
                   }
 
-                  if (drij_sq == 0.0)
+                  if (drij_sq == 0.0) // This should never be hit, but just in case
                   {
                     continue; // This is the same atom!
                   }
@@ -302,7 +327,7 @@ int main(int argc, char** argv)
   /****************************************************************************/
 
   // Now that we have the atoms safely stored, we can process them.
-  symm.resize(atoms.size(), 0);
+  symm.resize(atoms.size(), 0); // Assign each atom a symmetry parameter
   for (unsigned int i = 0; i < atoms.size(); ++i)
   {
     if (atoms[i].getType() != 1)
@@ -336,7 +361,7 @@ int main(int argc, char** argv)
       sintheta_sq = 1 - (rxij * rxij) / drij_sq;
       total += (coeffs[0] - coeffs[1] * sintheta_sq) * (coeffs[0] - coeffs[1] * sintheta_sq) * sintheta_sq;
     }
-    total /= iatom[0][i];
+    total /= iatom[0][i]; // This may not always be 12!
     symm[i] = total; // Store them for analysis
 
     if (atoms[i].getType() != 1)
@@ -346,13 +371,17 @@ int main(int argc, char** argv)
     if (symm[i] <= cutoff)
     {
       atoms[i].setMark(1);
+      ++n_grain_1;
     }
-    else
+    else // greater than the cutoff
     {
       atoms[i].setMark(2);
+      ++n_grain_2;
     }
   }
 
+  cout << "There are " << n_grain_1 << " U atoms in grain 1, and " << n_grain_2
+       << " U atoms in grain 2.\n";
   // Make sure we write the entire set of atoms
   // This writes things in a tecplot-readable format.
   n_atoms_read = 0;
