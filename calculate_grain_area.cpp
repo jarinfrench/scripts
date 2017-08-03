69e2559da30849926e3b1bee44d148ccaca4e912
Jarin French <jarin.french@live.com>
2017-08-03 10:16:55 -0400

    Modified the calculate_grain_area script to accomodate Cu and UO2 separately.

--- calculate_grain_area.cpp
+++ calculate_grain_area.cpp
@@ -3,32 +3,61 @@
 #include <sstream>
 #include <string>
 #include <cstdlib>
+#include <algorithm>
 
 using namespace std;
 
 #define PI 3.141592653589793
 
-double latticeParam(double T)
+double latticeParam(double T, string atom_type)
 {
   // A is the y intercept, B is the linear coefficient, C is the parabolic
   // coefficient
   double A, B, C;
-  if (T >= 0.0 && T <= 1000.0)
+  if (atom_type.compare("UO2"))
   {
-    A = 5.45297739;
-    B = 5.89383E-5;
-    C = 0.0;
+    if (T >= 0.0 && T <= 1000.0)
+    {
+      A = 5.45297739;
+      B = 5.89383E-5;
+      C = 0.0;
+    }
+    else if (T > 1000.0 && T <= 3300.00)
+    {
+      A = 5.44364863;
+      B = 6.18966E-5;
+      C = 5.27784E-9;
+    }
+    else
+    {
+      cout << "Temperature out of fitted range (0 K - 3300 K).\n";
+      exit(10); // We don't want to continue with execution if we're out of range.
+    }
   }
-  else if (T > 1000.0 && T <= 3300.00)
+  else if (atom_type.compare("CU"))
   {
-    A = 5.44364863;
-    B = 6.18966E-5;
-    C = 5.27784E-9;
+    if (T >= 0.0 && T <= 700.0)
+    {
+      A = 3.614;
+      B = 6.034E-5;
+      C = 0.0;
+    }
+    else if (T > 700.0 && T <= 1300.00)
+    {
+      A = 3.622;
+      B = 3.431E-5;
+      C = 2.31E-8;
+    }
+    else
+    {
+      cout << "Temperature out of fitted range (0 K - 1300 K).\n";
+      exit(10); // We don't want to continue with execution if we're out of range.
+    }
   }
   else
   {
-    cout << "Temperature out of fitted range (0 K - 3300 K).\n";
-    exit(10); // We don't want to continue with execution if we're out of range.
+    cout << "Only Cu and UO2 have been fitted.  Add to the latticeParam function if you want more data.\n";
+    exit(10);
   }
 
   return A + B * T + C * T * T;
@@ -36,14 +65,14 @@ double latticeParam(double T)
 
 int main(int argc, char **argv)
 {
-  string filename, filename2, str; // data file, junk variable.
+  string filename, filename2, atom_types, str; // data file, junk variable.
   double T, a0, area, scale_factor; // temperature, lattice parameter, area
   int N0, N1; // Number of atoms in grain 2 at time 1 and time 2
   double t0, t1, Lz; // time at 1, time at 2, height of cylindrical grain, GBE
   // NOTE: eGB is generally taken as 1.5.  I should use a value close to what
   // was calculated in the GBE calculations.
 
-  if (argc != 4)
+  if (argc != 5)
   {
     cout << "Please enter the filename containing the number of atoms in each grain: ";
     cin  >> filename;
@@ -53,15 +82,22 @@ int main(int argc, char **argv)
 
     cout << "Please enter the height of the original cylinder (in Angstroms): ";
     cin  >> Lz;
+
+    cout << "Please enter the molecules in the simulation (i.e. UO2, Cu): \n";
+    cin  >> atom_types;
   }
   else
   {
     filename = argv[1]; // get the filename
     T = strtod(argv[2], NULL); // get the temperature.
     Lz = strtod(argv[3], NULL); // get the cylinder height
+    atom_types = argv[4]; // get the molecule
   }
 
-  a0 = latticeParam(T);
+  // No matter what the user entered, capitalize everything.
+  transform(atom_types.begin(), atom_types.end(), atom_types.begin(), ::toupper);
+
+  a0 = latticeParam(T, atom_types);
   scale_factor = a0 / 5.453; // Important to account for expansion of the lattice
   Lz *= scale_factor;
 
