4d9fa8f6f56f837b93468c55163a443741a3dcef
Jarin French <jarin.french@live.com>
2017-10-20 09:10:56 -0400

    Modified the calculate_GBE script to have the option of calculating it for a spherical GB or a cylindrical GB.

--- calculate_GBE.cpp
+++ calculate_GBE.cpp
@@ -13,6 +13,7 @@
 #include <iomanip>
 #include <fstream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -21,21 +22,43 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-  string filename1, filename2;
+  string filename1, filename2, boundary_type;
+  bool is_sphere = false;
   int N0, N; // number of atoms for single grain, rotated grain
   double eGB, e_single, theta; // energy for rotated grain, single grain, angle of rotation
   double r_grain; //radius of the grain in angstroms
   double Lz; // thickness of the grain in angstroms
-  double gbe, bot; // grain boundary energy, denominator term to calculate gbe
+  double gbe, gb_area; // grain boundary energy, denominator term to calculate gbe
 
-  if (argc != 2)
+  if (argc != 3)
   {
     // Prompt for filename if we don't already have it.
     cout << "Please enter the filename containing the angle and energies: ";
     cin  >> filename1;
+
+    cout << "Please enter the boundary type (cylinder|sphere): ";
+    cin >> boundary_type;
   }
   else
+  {
     filename1 = argv[1];
+    boundary_type = argv[2];
+  }
+
+  transform(boundary_type.begin(), boundary_type.end(), boundary_type.begin(), ::tolower);
+  if (boundary_type.compare("cylinder") != 0 && boundary_type.compare("sphere") != 0)
+  {
+    cout << "Unable to process boundary type " << boundary_type << ", please enter either \"cylinder\" or \"sphere\"\n";
+    return 20;
+  }
+  else
+  {
+    if (boundary_type.compare("sphere") == 0)
+    {
+      is_sphere = true;
+    }
+  }
+
 
   ifstream fin(filename1.c_str());
   if (fin.fail())
@@ -49,8 +72,11 @@ int main(int argc, char **argv)
   cin  >> r_grain;
 
   //Prompt for the grain thickness
-  cout << "Please enter the grain thickness (Lz) in angstroms: ";
-  cin  >> Lz;
+  if (!is_sphere)
+  {
+    cout << "Please enter the grain thickness (Lz) in angstroms: ";
+    cin  >> Lz;
+  }
 
   // This assumes a filename given above in the format 111Tilt_total_energies.csv,
   // and creates a second file called 111Tilt_individual_energy.csv
@@ -62,7 +88,15 @@ int main(int argc, char **argv)
     return -1;
   }
 
-  bot = 2 * PI * r_grain * Lz; // area of the grain boundary
+  if (is_sphere)
+  {
+    gb_area = 4 * PI * r_grain * r_grain;
+  }
+  else // cylinder
+  {
+    gb_area = 2 * PI * r_grain * Lz; // area of the grain boundary
+  }
+
   // This assumes that the first line contains the original, single grain
   fin >> theta >> e_single >> N0;
   fout << theta << "," << setprecision(15) << 0.0 << endl;
@@ -71,7 +105,7 @@ int main(int argc, char **argv)
     if (fin.fail())
       cout << "Read error.\n";
 
-    gbe = (eGB - (e_single / N0) * N) / bot * eV2J; // Calculate the GBE
+    gbe = (eGB - (e_single / N0) * N) / gb_area * eV2J; // Calculate the GBE
     fout << theta << "," << setprecision(15) << gbe << endl; // Write to the new file
   }
 
