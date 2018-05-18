7b381ecc189dead67d1e5628159f70c191ce4eb2
Jarin French <jarin.french@live.com>
2018-05-18 14:22:19 -0400

    Modified the find_all_grains scripts to be one all-inclusive script

--- bash/find_all_grains.sh
+++ bash/find_all_grains.sh
@@ -1,13 +1,134 @@
 #! /bin/bash
 
-cd /media/jarinf/Seagate\ Expansion\ Drive/UO2/grain_growth
+read -p "Please enter the element: " element
+if [[ "${element,,}" =~ ^(al|cu|fe)$ ]]; then
+  element=${element,,}
+  element=${element^}
+elif [[ "${element,,}" =~ ^uo2$ ]]; then
+  element=${element^^}
+else
+  echo "$element is not recognized.  Please enter either UO2, Fe, Cu, or Al"
+  exit 3
+fi
+
+if [[ "${element}" =~ ^Fe$ ]]; then
+  potential=5
+  lattice_param=2.85532
+  cut100=0.933
+  cut110=0.933
+  cut111=0.933
+  cutoff=1.6
+  gamma=1.0
+elif [[ "${element}" =~ ^Al$ ]]; then
+  potential=4
+  lattice_param=4.032
+  cut100=0.866
+  cut110=0.866
+  cut111=1.2
+  cutoff=1.25
+  gamma=0.5
+elif [[ "${element}" =~ ^Cu$ ]]; then
+  potential=3
+  lattice_param=3.615
+  cut100=0.866
+  cut110=0.866
+  cut111=1.2
+  cutoff=1.25
+  gamma=0.9
+elif [[ "${element}" =~ ^UO2$ ]]; then
+  lattice_param=5.453
+  cut100=0.866
+  cut110=0.866
+  cut111=1.2
+  cutoff=1.25
+  gamma=1.6
+else
+  potential=0
+  read -p "Please enter the lattice parameter of the material: " lattice_param
+  read -p "Please enter the random grain boundary energy of this material: " gamma
+  while true; do
+    read -p "Is the material FCC (1), or BCC (2)? " crystal_type
+    if [[ "${crystal_type}" == 1 ]] || [[ "${crystal_type}" == 2 ]]; then
+      break
+    fi
+  done
+  if [[ "${crystal_type}" == 1 ]]; then
+    cut100=0.866
+    cut110=0.866
+    cut111=1.2
+    cutoff=1.25
+  elif [[ "${crystal_type}" == 2 ]]; then
+    cut100=0.933
+    cut110=0.933
+    cut111=0.933
+    cutoff=1.6
+  else
+    echo "Error determining crystal type."
+    exit 4
+  fi
+fi
+
+cd /media/jarinf/Seagate\ Expansion\ Drive/${element}/grain_growth
 
 for i in 100 110 111; do
-  cd ${i}/45degree
-  for j in basak eam; do
-    cd ${j}
-    for k in $(ls -vd T*/); do
-      cd ${k}
+  cd ${i}
+  if ! [[ "${element}" =~ ^UO2$ ]]; then
+    for j in $(ls -vd T*/); do
+      cd ${j}
+      for k in $(ls -d dir*/); do
+        cd ${k}
+        echo -e "\nProcessing directory ${i}/${j}${k}"
+        if [ -d "interfaces" ]; then
+          echo "Directory \"interfaces\" already found in ${i}/${j}${k}"
+          t=$(echo $j | cut -c 2- | awk -F '/' '{print $1}')
+          height=$(cat ${element}_minimized_* | head -n 7 | tail -n 1 | awk '{print $2}')
+          calculate_grain_area data.txt $t $height ${lattice_param} $potential
+          calculate_force_and_velocity.py $t ${height} ${lattice_param} ${gamma} -p $potential
+        else
+          if [ "$(ls *.dump 2>/dev/null | wc -l)" -eq 0 ]; then
+            cd ..
+            continue
+          fi
+          if [ "$i" -eq 100 ]; then
+            echo "$(ls *.dump | wc -l) 45.00 1 ${cut100} ${cutoff} ${lattice_param}" > base_vals.txt
+            echo "1 0 0" >> base_vals.txt
+            echo "0 1 0" >> base_vals.txt
+            echo "0 0 1" >> base_vals.txt
+          elif [ "$i" -eq 110 ]; then
+            echo "$(ls *.dump | wc -l) 45.00 1 ${cut110} ${cutoff} ${lattice_param}" > base_vals.txt
+            echo "0 0 1" >> base_vals.txt
+            echo "1 -1 0" >> base_vals.txt
+            echo "1 1 0" >> base_vals.txt
+          elif [ "$i" -eq 111 ]; then
+            echo "$(ls *.dump | wc -l) 45.00 1 ${cut111} ${cutoff} ${lattice_param}" > base_vals.txt
+            echo "1 -1 0" >> base_vals.txt
+            echo "1 1 -2" >> base_vals.txt
+            echo "1 1 1" >> base_vals.txt
+          fi
+          ls -v *.dump >> base_vals.txt
+          find_grains base_vals.txt
+          mkdir interfaces
+          mv *_interface* interfaces
+          if ! [ -a "area_data.txt" ]; then
+            t=$(echo $j | cut -c 2- | awk -F '/' '{print $1}')
+            height=$(cat ${element}_minimized_* | head -n 7 | tail -n 1 | awk '{print $2}')
+            calculate_grain_area data.txt $t ${height} ${lattice_param} $potential
+            calculate_force_and_velocity.py $t ${height} ${lattice_param} ${gamma} -p $potential
+          else
+            echo -e "\033[0;32m\tArea data file already found in ${i}/${j}${k}\033[0m"
+          fi
+        fi
+        cd ..
+      done
+      cd ..
+    done
+    cd ..
+  else
+    cd 45degree
+    for j in basak eam; do
+      cd ${j}
+      for k in $(ls -vd T*/); do
+        cd ${k}
         for l in large_r medium_r small_r tiny_r; do
           if [ -d "$l" ]; then
             cd ${l}
@@ -25,8 +146,8 @@ for i in 100 110 111; do
                 elif [ "$j" == "eam" ]; then
                   potential=1
                 fi
-                calculate_grain_area data.txt $t ${height} 5.453 ${potential}
-                calculate_force_and_velocity.py $t ${height} 5.453 -p ${potential}
+                calculate_grain_area data.txt $t ${height} ${lattice_param} ${potential}
+                calculate_force_and_velocity.py $t ${height} ${lattice_param} ${gamma} -p ${potential}
                 echo -e "\n"
               else
                 if [ "$(ls *.dump 2>/dev/null | wc -l)" -eq 0 ]; then
@@ -34,17 +155,17 @@ for i in 100 110 111; do
                   continue
                 fi
                 if [ "$i" -eq 100 ]; then
-                  echo "$(ls *.dump | wc -l) 45.00 2 0.866 1.25 5.453" > base_vals.txt
+                  echo "$(ls *.dump | wc -l) 45.00 2 ${cut100} ${cutoff} ${lattice_param}" > base_vals.txt
                   echo "1 0 0" >> base_vals.txt
                   echo "0 1 0" >> base_vals.txt
                   echo "0 0 1" >> base_vals.txt
                 elif [ "$i" -eq 110 ]; then
-                  echo "$(ls *.dump | wc -l) 45.00 2 0.866 1.25 5.453" > base_vals.txt
+                  echo "$(ls *.dump | wc -l) 45.00 2 ${cut110} ${cutoff} ${lattice_param}" > base_vals.txt
                   echo "0 0 1" >> base_vals.txt
                   echo "1 -1 0" >> base_vals.txt
                   echo "1 1 0" >> base_vals.txt
                 elif [ "$i" -eq 111 ]; then
-                  echo "$(ls *.dump | wc -l) 45.00 2 1.2 1.25 5.453" > base_vals.txt
+                  echo "$(ls *.dump | wc -l) 45.00 2 ${cut111} ${cutoff} ${lattice_param}" > base_vals.txt
                   echo "1 -1 0" >> base_vals.txt
                   echo "1 1 -2" >> base_vals.txt
                   echo "1 1 1" >> base_vals.txt
@@ -61,8 +182,8 @@ for i in 100 110 111; do
                 elif [ "$j" == "eam" ]; then
                   potential=1
                 fi
-                calculate_grain_area data.txt $t ${height} 5.453 ${potential}
-                calculate_force_and_velocity.py $t ${height} 5.453 -p ${potential}
+                calculate_grain_area data.txt $t ${height} ${lattice_param} ${potential}
+                calculate_force_and_velocity.py $t ${height} ${lattice_param} ${gamma} -p ${potential}
                 echo -e "\n"
               fi
               cd ..
@@ -70,9 +191,11 @@ for i in 100 110 111; do
             cd ..
           fi
         done
+        cd ..
+      done
       cd ..
     done
     cd ..
-  done
-  cd ../..
+  cd ..
+  fi
 done
