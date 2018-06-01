e75fe28e525952cd395599ac01bd73dc6541cee6
Jarin French <jarin.french@live.com>
2018-06-01 13:30:17 -0400

    Fixed the xyz2dat script to handle non-cubic systems.

--- src/xyz2dat.cpp
+++ src/xyz2dat.cpp
@@ -8,8 +8,19 @@
 #include <fstream>
 #include <string>
 #include <map>
+#include <vector>
+#include <cmath> // for cos/sin
+#include <algorithm> // for min/max_element
+#include <complex>
+#include "atom.h"
+
+#define PI 3.14159265358979
 
 using namespace std;
+
+bool compareAtomsX(const Atom &a, const Atom &b) {return a.getX() < b.getX();}
+bool compareAtomsY(const Atom &a, const Atom &b) {return a.getY() < b.getY();}
+bool compareAtomsZ(const Atom &a, const Atom &b) {return a.getZ() < b.getZ();}
 
 int main(int argc, char** argv)
 {
@@ -17,8 +28,12 @@ int main(int argc, char** argv)
   int n_atoms, N, type, n_types;
   double xlow, xhigh, ylow, yhigh, zlow, zhigh;
   double x, y, z, charge, temp = 1.0;
+  double Lx, Ly, Lz; // Box lengths in each direction
+  double a, b, c, alpha, beta, gamma; // lattice parameters, lattice angles
   string atom_type, chem_formula;
   map <string, int> elements;
+  vector <Atom> atoms;
+  bool show_charge = false;
 
   if (argc == 1)
   {
@@ -30,53 +45,65 @@ int main(int argc, char** argv)
     filename1 = argv[1];
   }
 
+  // Determine where in the filename the element is specified.  Assumes a filename
+  // format of "LAMMPS_<element(s)>_N<number_of_atoms>[_<extra_info>].dat"
   size_t element_pos = filename1.find("LAMMPS_")+7;
   size_t element_pos_end;
   { // Create a local namespace so we only store the results of this subroutine
-    size_t temp = filename1.find("_N");
+    size_t temp = filename1.find("_N"); // Find where the '_N' is.
+     // There are some elements that begin with N, so we check to make sure we
+     // found the actual element list
     temp = filename1.find("_N", temp + 1);
+    // if the temp variable does not find any other '_N' strings in the filename...
     if (temp == string::npos)
     {
+      // The end of the elements position is where the '_N' string begins
       element_pos_end = filename1.find("_N");
     }
     else
     {
+      // Otherwise, the end of the elements position is where the second _N string begins.
       element_pos_end = temp;
     }
   }
 
-  if (element_pos == string::npos)
+  if (element_pos == string::npos) // Error if we didn't find the 'LAMMPS_' string in the filename
   {
-    cout << "Error determining element(s).\n";
+    cout << "Error determining element(s).  Make sure the filename is in the format\n"
+         << "\t\"LAMMPS_<element(s)>_N<number_of_atoms>[_<extra_info>].dat\"\n"
+         << "Note that elements should be properly capitalized, i.e. table salt would be listed as NaCl.\n";
     return 8;
   }
-  else
+  else // Determine the number of elements.  Note that because we are using a map, duplicated elements will not appear more than once in the list
   {
     int elem_num = 1;
     for (unsigned int i = element_pos; i < element_pos_end; ++i)
     {
-      if (islower(filename1[i]))
+      if (islower(filename1[i])) // if the current letter is lowercase
       {
-        if ((elements.insert(pair<string,int>(filename1.substr(i-1,2), elem_num))).second) // check to see if insertion was successful
+        // Our element is the character before plus this character
+        if ((elements.insert(pair<string,int>(filename1.substr(i-1,2), elem_num))).second) // check to see if insertion was successful - fails if a duplicate
         {
           ++elem_num;
         }
       }
-      else
+      else // current letter is uppercase
       {
-        if (islower(filename1[i+1]))
+        if (islower(filename1[i+1])) // if the next character is lowercase, we handle that in the next loop
         {
           continue;
         }
         else
         {
+          // if the current character is a number, move ahead
+          // we don't need to calculate the element ratios
           if (isdigit(filename1[i]))
           {
             continue;
           }
-          else
+          else // Otherwise, we have a single-letter element (like O for oxygen)
           {
-            if ((elements.insert(pair<string, int> (filename1.substr(i,1), elem_num))).second) // check to see if insertion was successful
+            if ((elements.insert(pair<string, int> (filename1.substr(i,1), elem_num))).second) // check to see if insertion was successful - fails if a duplicate
             {
               ++elem_num;
             }
@@ -89,8 +116,7 @@ int main(int argc, char** argv)
 
   n_types = elements.size();
 
-  filename2 = "temp.dat"; // temporary filename until we determine what it needs to be
-  filename3 = filename1.substr(0, filename1.find(".")) + ".dat";
+  filename2 = filename1.substr(0, filename1.find(".")) + ".dat";
 
   ifstream fin(filename1.c_str());
   if (fin.fail())
@@ -110,19 +136,11 @@ int main(int argc, char** argv)
 
   fin >> N; // Number of atoms
   fout << N << "  atoms\n";
+  atoms.resize(N); // pre-allocate the vector size to save time and space.
 
   fout << n_types << "   atom types\n";
 
-  fin >> xhigh >> yhigh >> zhigh >> str >> str >> str; // Boundaries
-  xlow = 0.0;
-  ylow = 0.0;
-  zlow = 0.0;
-  fout << fixed;
-  fout << setprecision(6) << xlow << " " << xhigh << " xlo xhi\n";
-  fout << ylow << " " << yhigh << " ylo yhi\n";
-  fout << zlow << " " <<  zhigh << " zlo zhi\n";
-
-  fout << "\nAtoms\n\n";
+  fin >> Lx >> Ly >> Lz >> alpha >> beta >> gamma; // Boundaries
 
   n_atoms = 0;
   while (fin >> atom_type >> x >> y >> z) // Read in the data
@@ -132,106 +150,110 @@ int main(int argc, char** argv)
     {
       type = 1;
       charge = 2.4;
+      show_charge = true;
     }
     else if (atom_type.compare("O") == 0) // Same with the O atom
     {
       type = 2;
       charge = -1.2;
+      show_charge = true;
     }
     else
     {
       type = elements[atom_type];
       charge = 0.0; // This will have to be manually fixed later if there is a charge
     }
-
-    // This just double checks that our bounds are set correctly.  If not, they
-    // will be correct later.
-    if (x < xlow)
-    {
-      temp = -xhigh;
-      xhigh = 0.0;
-      xlow = temp;
-    }
-    if (y < ylow)
-    {
-      temp = -yhigh;
-      yhigh = 0.0;
-      ylow = temp;
-    }
-    if (z < zlow)
-    {
-      temp = -zhigh;
-      zhigh = 0.0;
-      zlow = temp;
-    }
-
-    fout << setprecision(0) << ++n_atoms << " " << type << " ";
-
-    if (charge != 0.0)
-    {
-      fout << setprecision(1) << charge << " ";
-    }
-    fout << setprecision(6) << x << " " << y << " " << z << endl;
+    ++n_atoms;
+    Atom tmp = Atom(n_atoms, type, charge, x, y, z);
+    atoms[n_atoms - 1] = tmp;
   }
-
-  // Close the file streams
-  fin.close();
-  fout.close();
   if (n_atoms != N)
   {
     cout << "Error counting atoms: n_atoms = " << n_atoms << " != N = " << N << endl;
     return 3;
   }
 
-  // Now fix the bounds if necessary
-  if (temp < 0) // This works because we initialize temp as 1.0, and throughout this code it is ONLY negative
-  {
-    ifstream fin(filename2.c_str());
-    if (fin.fail())
-    {
-      cout << "Unable to open file " << filename2 << endl;
-      return 1;
-    }
-    ofstream fout(filename3.c_str());
-    if (fout.fail())
-    {
-      cout << "Unable to open file " << filename3 << endl;
-      return 1;
-    }
-    fout << fixed;
+  xlow = (*min_element(atoms.begin(), atoms.end(), compareAtomsX)).getX();
+  ylow = (*min_element(atoms.begin(), atoms.end(), compareAtomsY)).getY();
+  zlow = (*min_element(atoms.begin(), atoms.end(), compareAtomsZ)).getZ();
+  xhigh = xlow + Lx;
+  yhigh = ylow + Ly;
+  zhigh = zlow + Lz;
 
-    // We need to re-write everything to the new file.
-    getline(fin, str); // Comment line
-    fout << str << "\n\n";
-    getline(fin,str); // blank line
-    getline(fin, str); // N atoms line
-    fout << str << "\n";
-    getline(fin, str); // n atom types line
-    fout << str << "\n";
-    fin >> str >> str >> str >> str; // x bounds
-    fout << setprecision(6) << xlow << " " << xhigh << " xlo xhi\n";
-    fin >> str >> str >> str >> str; // y bounds
-    fout << ylow << " " << yhigh << " ylo yhi\n";
-    fin >> str >> str >> str >> str; // z bounds
-    fout << zlow << " " << zhigh << " zlo zhi";
-    fin.clear();
-    while (getline(fin,str))
-    {
-      fout << str << "\n";
-    }
-    if (remove(filename2.c_str()) != 0)
-    {
-      cout << "Error deleting temp file \"temp.dat\"\n";
-    }
-    // Close the file streams
-    fin.close();
-    fout.close();
-  }
-  // If the bounds haven't changed, just rename the file (a LOT easier!)
-  else // rename the temp file.
+  fout << fixed;
+  fout << setprecision(6) << xlow << " " << xhigh << " xlo xhi\n";
+  fout << ylow << " " << yhigh << " ylo yhi\n";
+  fout << zlow << " " <<  zhigh << " zlo zhi\n";
+
+  if (alpha != 90.00 || beta != 90.00 || gamma != 90.00)
   {
-    rename(filename2.c_str(), filename3.c_str());
+    complex <double> xy, xz, yz, cos_a, cos_b, cos_g, sin_g; // Tilt parameters, sines and cosines
+    complex <double> ii;
+    ii.imag(1); // the square root of -1
+    double ly;
+
+    cout << "Triclinic box detected; calculating tilt parameters.\n";
+    cos_a = cos(alpha * PI / 180.0);
+    cos_b = cos(beta * PI / 180.0);
+    cos_g = cos(gamma * PI / 180.0);
+    sin_g = sin(gamma * PI / 180.0);
+
+    if (sin_g.real() < 1E-8 && sin_g.imag() < 1E-8)
+    {
+      cout << "Error: z cannot be coplanar with the xy plane!  Gamma must be > 0 and < 180\n";
+    }
+    else
+    {
+      if (gamma == 90.00)
+      {
+        xy = 0.0;
+      }
+      else
+      {
+        xy = Ly * cos_g / sqrt(sin_g * sin_g);
+      }
+
+      if (beta == 90.00)
+      {
+        xz = 0.0;
+      }
+      else
+      {
+        xz = (cos_b * ii * sqrt(Lz * Lz * sin_g * sin_g)) / \
+            (sqrt(-1.0 + cos_a * cos_a + cos_b + cos_b + cos_g * cos_g - 2.0 * cos_a * cos_b * cos_g));
+      }
+      yz = ((cos_a - cos_b * cos_g) * ii * sqrt(Lz * sin_g * sin_g)) / \
+            (sqrt(-1.0 + cos_a * cos_a + cos_b + cos_b + cos_g * cos_g - 2.0 * cos_a * cos_b * cos_g) * sqrt(sin_g * sin_g));
+    }
+
+    if (xy.imag() > 1E-8 || xz.imag() > 1E-8 || yz.imag() > 1E-8)
+    {
+      cout << "Error: imaginary value determined.\n";
+      return 10;
+    }
+    else
+    {
+      fout << xy.real() << " " << xz.real() << " " << yz.real() << " xy xz yz\n";
+    }
   }
+
+  fout << "\nAtoms\n\n";
+
+  for (unsigned int i = 0; i < atoms.size(); ++i)
+  {
+    fout << setprecision(0) << atoms[i].getId() << " " << atoms[i].getType() << " ";
+
+    if (show_charge)
+    {
+      fout << setprecision(1) << atoms[i].getCharge() << " ";
+    }
+    fout << setprecision(6) << atoms[i].getX() << " " << atoms[i].getY() << " " << atoms[i].getZ() << endl;
+  }
+
+  // Close the file streams
+  fin.close();
+  fout.close();
+
 
   return 0;
 }
