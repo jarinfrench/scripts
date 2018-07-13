2935b3ca476b5e3f6ba293220a94c02368110935
Jarin French <jarin.french@live.com>
2018-07-13 16:06:39 -0400

    Modified xyz2dat to use cxxopts.  Can now manage files with unnamed elements.

--- src/xyz2dat.cpp
+++ src/xyz2dat.cpp
@@ -16,90 +16,143 @@
 #include "atom.h"
 
 #define PI 3.14159265358979
+#define FILE_OPEN_ERROR 1
+#define FILE_FORMAT_ERROR 2
+#define ATOM_COUNT_ERROR 3
+#define IMAGINARY_TILT_ERROR 4
 
 using namespace std;
 
+// various comparison functions for the atoms
 bool compareAtomsX(const Atom &a, const Atom &b) {return a.getX() < b.getX();}
 bool compareAtomsY(const Atom &a, const Atom &b) {return a.getY() < b.getY();}
 bool compareAtomsZ(const Atom &a, const Atom &b) {return a.getZ() < b.getZ();}
 
-int main(int argc, char** argv)
+vector <double> determineTiltParameters(const double& Lx, const double& Ly,
+                                        const double& Lz, const double& alpha,
+                                        const double& beta, const double& gamma)
 {
-  string filename1, filename2, filename3, str;
-  int n_atoms, N, type, n_types;
-  double xlow, xhigh, ylow, yhigh, zlow, zhigh;
-  double x, y, z, charge, temp = 1.0;
-  double Lx, Ly, Lz; // Box lengths in each direction
-  double a, b, c, alpha, beta, gamma; // lattice parameters, lattice angles
-  string atom_type, chem_formula;
-  map <string, int> elements;
-  vector <Atom> atoms;
-  bool show_charge = false;
+  double x = Lx, y = Ly, z = Lz;
+  complex <double> xy, xz, yz, cos_a, cos_b, cos_g, sin_g; // tilt parameters, (co)sines of the angles
+  complex <double> ii;
+  ii.imag(1); // the square root of -1
+  double ly;
 
-  // cxxopts::Options options(argv[0], "Convert a file in the xyz format to the dat format");
-  // options.add_options()
-  //   ("file", "File name", cxxopts::value<string>())
-  //   ("s,show-charge", "Whether or not to write the charge values", cxxopts::value<bool>()->default_value("false"))
-  //   ("n,no-elements", "Flag specifying that the filename does not contain the element(s)", cxxopts::value<bool>(0->default_value("false")));
-  //
-  // options.parse_positionals("file")
-  // auto args = options.parse(argc, argv);
+  cout << "Triclinic box detected; calculating tilt parameters.\n";
+  cos_a = cos(alpha * PI / 180.0);
+  cos_b = cos(beta * PI / 180.0);
+  cos_g = cos(gamma * PI / 180.0);
+  sin_g = sin(gamma * PI / 180.0);
 
-  if (argc == 1)
+  // This needs some work... it depends on the orientation of the parallelepiped
+  x = x + (y * sin(abs(90.00 - gamma) * PI / 180.0)) + z * (cos_b.real());
+  y = 1.0;
+
+  if (sin_g.real() < 1E-8 && sin_g.imag() < 1E-8)
   {
-    cout << "Please enter the file to be converted: ";
-    cin >> filename1;
+    cout << "Error: z cannot be coplanar with the xy plane!  Gamma must be > 0 and < 180\n";
   }
   else
   {
-    filename1 = argv[1];
-  }
-
-  // Determine where in the filename the element is specified.  Assumes a filename
-  // format of "LAMMPS_<element(s)>_N<number_of_atoms>[_<extra_info>].dat"
-  size_t element_pos = filename1.find("LAMMPS_")+7;
-  size_t element_pos_end;
-  { // Create a local namespace so we only store the results of this subroutine
-    size_t temp = filename1.find("_N"); // Find where the '_N' is.
-     // There are some elements that begin with N, so we check to make sure we
-     // found the actual element list
-    temp = filename1.find("_N", temp + 1);
-    // if the temp variable does not find any other '_N' strings in the filename...
-    if (temp == string::npos)
+    if (gamma == 90.00)
     {
-      // The end of the elements position is where the '_N' string begins
-      element_pos_end = filename1.find("_N");
+      xy = 0.0;
     }
     else
     {
-      // Otherwise, the end of the elements position is where the second _N string begins.
+      xy = y * cos_g / sqrt(sin_g * sin_g);
+    }
+
+    if (beta == 90.00)
+    {
+      xz = 0.0;
+    }
+    else
+    {
+      xz = (cos_b * ii * sqrt(Lz * Lz * sin_g * sin_g)) / \
+          (sqrt(-1.0 + cos_a * cos_a + cos_b + cos_b + cos_g * cos_g - 2.0 * cos_a * cos_b * cos_g));
+    }
+    yz = ((cos_a - cos_b * cos_g) * ii * sqrt(Lz * sin_g * sin_g)) / \
+          (sqrt(-1.0 + cos_a * cos_a + cos_b + cos_b + cos_g * cos_g - 2.0 * cos_a * cos_b * cos_g) * sqrt(sin_g * sin_g));
+  }
+
+  if (xy.imag() > 1E-8 || xz.imag() > 1E-8 || yz.imag() > 1E-8)
+  {
+    cout << "Error: imaginary value determined.\n";
+    exit(IMAGINARY_TILT_ERROR);
+  }
+  else
+  {
+    vector <double> tilt_params(3,0.0);
+    tilt_params[0] = xy.real();
+    tilt_params[1] = xz.real();
+    tilt_params[2] = yz.real();
+    return tilt_params;
+  }
+}
+
+string determineElement(const string& file)
+{
+  // Filename must be in the format "LAMMPS_<element(s)>_N<number_of_atoms>[_<extra_info>].dat"
+  size_t element_pos_start = file.find("LAMMPS_") + 7;
+  size_t element_pos_end;
+  { // Local namespace so we only store the results
+    size_t temp = file.find("_N");
+    temp = file.find("_N", temp + 1); // double check that we found the number of atoms specification, as some compounds do begin with "_N"
+    // If there are no other cases of "_N" in the filename, we get the end of the string
+    if (temp == string::npos)
+    {
+      element_pos_end = file.find("_N");
+    }
+    else
+    {
       element_pos_end = temp;
     }
   }
 
-  if (element_pos == string::npos) // Error if we didn't find the 'LAMMPS_' string in the filename
+  if (element_pos_start == string::npos)
   {
     cout << "Error determining element(s).  Make sure the filename is in the format\n"
          << "\t\"LAMMPS_<element(s)>_N<number_of_atoms>[_<extra_info>].dat\"\n"
-         << "Note that elements should be properly capitalized, i.e. table salt would be listed as NaCl.\n";
-    return 8;
+         << "Note that elements should be properly capitalized, i.e. table salt would be NaCl.\n";
+    exit(FILE_FORMAT_ERROR);
   }
   else // Determine the number of elements.  Note that because we are using a map, duplicated elements will not appear more than once in the list
   {
+    return file.substr(element_pos_start, element_pos_end - element_pos_start);
+  }
+}
+
+void convertFile(const string& file, const bool show_charge, const bool no_element_listed)
+{
+  string file2; // second file (we write to this one)
+  string chem_formula = "<element>"; // chemical formula
+  map <string, int> elements; // map of element name to element number
+  int n_types = 1, N, type, n_atoms; // number of atom types, number of atoms, atom type, number of atoms read
+  vector <Atom> atoms; // container for the atoms
+  double Lx, Ly, Lz, alpha, beta, gamma; // Box bounds, angles
+  string atom_type; // atom type listed in the original file
+  double x, y, z, charge; // atom x, y, and z positions in the file, charge of atom
+  double xlow, ylow, zlow, xhigh, yhigh, zhigh; // min and max of atom positions
+
+  if (!no_element_listed) // If an element is specified in the filename, find it
+  {
+    chem_formula = determineElement(file);
     int elem_num = 1;
-    for (unsigned int i = element_pos; i < element_pos_end; ++i)
+    for (unsigned int i = 0; i < chem_formula.size(); ++i)
     {
-      if (islower(filename1[i])) // if the current letter is lowercase
+      if (islower(chem_formula[i])) // if the current letter is lowercase
       {
         // Our element is the character before plus this character
-        if ((elements.insert(pair<string,int>(filename1.substr(i-1,2), elem_num))).second) // check to see if insertion was successful - fails if a duplicate
+        // check to see if insertion was successful - fails if a duplicate
+        if ((elements.insert(pair<string,int>(chem_formula.substr(i-1,2), elem_num))).second)
         {
           ++elem_num;
         }
       }
       else // current letter is uppercase
       {
-        if (islower(filename1[i+1])) // if the next character is lowercase, we handle that in the next loop
+        if (islower(chem_formula[i+1])) // if the next character is lowercase, we handle that in the next loop
         {
           continue;
         }
@@ -107,13 +160,13 @@ int main(int argc, char** argv)
         {
           // if the current character is a number, move ahead
           // we don't need to calculate the element ratios
-          if (isdigit(filename1[i]))
+          if (isdigit(chem_formula[i]))
           {
             continue;
           }
           else // Otherwise, we have a single-letter element (like O for oxygen)
           {
-            if ((elements.insert(pair<string, int> (filename1.substr(i,1), elem_num))).second) // check to see if insertion was successful - fails if a duplicate
+            if ((elements.insert(pair<string, int> (chem_formula.substr(i,1), elem_num))).second)
             {
               ++elem_num;
             }
@@ -121,28 +174,33 @@ int main(int argc, char** argv)
         }
       }
     }
+    n_types = elements.size();
   }
-  chem_formula = filename1.substr(element_pos, element_pos_end - element_pos);
 
-  n_types = elements.size();
+  file2 = file.substr(0,file.find(".")) + ".dat"; // Assumes only one "." symbol in file.
 
-  filename2 = filename1.substr(0, filename1.find(".")) + ".dat";
-
-  ifstream fin(filename1.c_str());
+  ifstream fin(file.c_str());
   if (fin.fail())
   {
-    cout << "Unable to open file " << filename1 << endl;
-    return 1;
+    cout << "Unable to open file " << file << endl;
+    exit(FILE_OPEN_ERROR);
   }
 
-  ofstream fout(filename2.c_str());
+  ofstream fout(file2.c_str());
   if (fout.fail())
   {
-    cout << "Unable to open file " << filename2 << endl;
-    return 1;
+    cout << "Unable to open file " << file2 << endl;
+    exit(FILE_OPEN_ERROR);
   }
 
-  fout << "This bulk " << chem_formula << " coordinates format: [ID type charge* x y z] (* not included if always charge neutral)\n\n";
+  if (!show_charge)
+  {
+    fout << "This bulk " << chem_formula << " coordinates format: [ID type x y z]\n\n";
+  }
+  else
+  {
+    fout << "This bulk " << chem_formula << " coordinates format: [ID type charge x y z]\n\n";
+  }
 
   fin >> N; // Number of atoms
   fout << N << "  atoms\n";
@@ -150,36 +208,34 @@ int main(int argc, char** argv)
 
   fout << n_types << "   atom types\n";
 
-  fin >> Lx >> Ly >> Lz >> alpha >> beta >> gamma; // Boundaries
+  fin >> Lx >> Ly >> Lz >> alpha >> beta >> gamma; // boundaries and angles
 
   n_atoms = 0;
   while (fin >> atom_type >> x >> y >> z) // Read in the data
   {
-    if (atom_type.compare("U") == 0) // Given a U atom, write the correct type and charge
+    if (atom_type.compare("U") == 0)
     {
       type = 1;
       charge = 2.4;
-      //show_charge = true;
     }
-    else if (atom_type.compare("O") == 0) // Same with the O atom
+    else if (atom_type.compare("O") == 0)
     {
       type = 2;
       charge = -1.2;
-      show_charge = true;
     }
     else
     {
       type = elements[atom_type];
-      charge = 0.0; // This will have to be manually fixed later if there is a charge
+      charge = 0.0; // Needs to be manually fixed later if there is a charge
     }
     ++n_atoms;
-    Atom tmp = Atom(n_atoms, type, charge, x, y, z);
-    atoms[n_atoms - 1] = tmp;
+    atoms[n_atoms - 1] = Atom(n_atoms, type, charge, x, y, z);
   }
+
   if (n_atoms != N)
   {
     cout << "Error counting atoms: n_atoms = " << n_atoms << " != N = " << N << endl;
-    return 3;
+    exit(ATOM_COUNT_ERROR);
   }
 
   xlow = (*min_element(atoms.begin(), atoms.end(), compareAtomsX)).getX();
@@ -196,62 +252,9 @@ int main(int argc, char** argv)
 
   if (alpha != 90.00 || beta != 90.00 || gamma != 90.00)
   {
-    x = Lx;
-    y = Ly;
-    z = Lz;
-
-    complex <double> xy, xz, yz, cos_a, cos_b, cos_g, sin_g; // Tilt parameters, sines and cosines
-    complex <double> ii;
-    ii.imag(1); // the square root of -1
-    double ly;
-
-    cout << "Triclinic box detected; calculating tilt parameters.\n";
-    cos_a = cos(alpha * PI / 180.0);
-    cos_b = cos(beta * PI / 180.0);
-    cos_g = cos(gamma * PI / 180.0);
-    sin_g = sin(gamma * PI / 180.0);
-
-    // This needs some work... it depends on the orientation of the parallelepiped
-    Lx = x + (y * sin(abs(90.00 - gamma) * PI / 180.0)) + z * (cos_b.real());
-    Ly = 1.0;
-
-    if (sin_g.real() < 1E-8 && sin_g.imag() < 1E-8)
-    {
-      cout << "Error: z cannot be coplanar with the xy plane!  Gamma must be > 0 and < 180\n";
-    }
-    else
-    {
-      if (gamma == 90.00)
-      {
-        xy = 0.0;
-      }
-      else
-      {
-        xy = Ly * cos_g / sqrt(sin_g * sin_g);
-      }
-
-      if (beta == 90.00)
-      {
-        xz = 0.0;
-      }
-      else
-      {
-        xz = (cos_b * ii * sqrt(Lz * Lz * sin_g * sin_g)) / \
-            (sqrt(-1.0 + cos_a * cos_a + cos_b + cos_b + cos_g * cos_g - 2.0 * cos_a * cos_b * cos_g));
-      }
-      yz = ((cos_a - cos_b * cos_g) * ii * sqrt(Lz * sin_g * sin_g)) / \
-            (sqrt(-1.0 + cos_a * cos_a + cos_b + cos_b + cos_g * cos_g - 2.0 * cos_a * cos_b * cos_g) * sqrt(sin_g * sin_g));
-    }
-
-    if (xy.imag() > 1E-8 || xz.imag() > 1E-8 || yz.imag() > 1E-8)
-    {
-      cout << "Error: imaginary value determined.\n";
-      return 10;
-    }
-    else
-    {
-      fout << xy.real() << " " << xz.real() << " " << yz.real() << " xy xz yz\n";
-    }
+    vector <double> tilt_params(3,0.0);
+    tilt_params = determineTiltParameters(Lx,Ly,Lz,alpha,beta,gamma);
+    fout << tilt_params[0] << " " << tilt_params[1] << " " << tilt_params[2] << " xy xz yz\n";
   }
 
   fout << "\nAtoms\n\n";
@@ -264,13 +267,58 @@ int main(int argc, char** argv)
     {
       fout << setprecision(1) << atoms[i].getCharge() << " ";
     }
+
     fout << setprecision(6) << atoms[i].getX() << " " << atoms[i].getY() << " " << atoms[i].getZ() << endl;
   }
 
   // Close the file streams
   fin.close();
   fout.close();
+}
 
+int main(int argc, char** argv)
+{
+  bool show_charge = false;
+  bool no_element_listed = false;
+
+  try
+  {
+    cxxopts::Options options(argv[0], "Convert a file in the xyz format to the dat format");
+    options
+      .positional_help("File")
+      .show_positional_help();
+
+    options
+      .allow_unrecognised_options()
+      .add_options()
+        ("f,file", "Input file", cxxopts::value<vector<string> >(), "file")
+        ("s,show-charge", "Include charge in the converted file", cxxopts::value<bool>(show_charge)->default_value("false"))
+        ("n,no-element", "Ignores presence of element names", cxxopts::value<bool>(no_element_listed)->default_value("false"))
+        ("h,help", "Show the help");
+
+    options.parse_positional({"file"});
+    auto result = options.parse(argc, argv);
+
+    if (result.count("help"))
+    {
+      cout << options.help() << endl;
+      return 0;
+    }
+
+    if (result.count("file"))
+    {
+      auto& filenames = result["file"].as<vector<string> >();
+      for (const auto& file : filenames)
+      {
+        convertFile(file, show_charge, no_element_listed);
+      }
+    }
+  }
+  catch (const cxxopts::OptionException& e)
+  {
+    cout << "Error parsing options: " << e.what() << endl;
+    return 1;
+  }
 
   return 0;
 }
