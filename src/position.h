879d2bdfb5e590e38c1ba5b0b2f5c23f037b362c
Jarin French <jarin.french@inl.gov>
2019-07-24 11:20:34 -0600

    Modified phase_field to use to the Position class, modified makefile accordingly.

--- src/position.h
+++ src/position.h
@@ -16,6 +16,7 @@ private:
 
 public:
   Position(); // Default Constructor
+  Position(double x, double y);
   Position(double x, double y, double z); // Constructor given the coordinates
 
   double getX() const {return x;}
@@ -25,12 +26,47 @@ public:
   void setX(double x) {this->x = x;}
   void setY(double y) {this->y = y;}
   void setZ(double z) {this->z = z;}
+  
+  void print2D() {std::cout << "(" << this->x << "," << this->y << ")";}
 };
 
-Position operator -(const Position& rhs); // unary minus
-Position operator +(const Position& lhs, const Position& rhs);
-Position operator -(const Position& lhs, const Position& rhs);
+// Non member operator overloads
 std::ostream& operator << (std::ostream&, const Position&);
 std::istream& operator >> (std::istream&, Position&);
 bool operator==(const Position& lhs, const Position& rhs);
+bool operator!=(const Position& lhs, const Position& rhs);
+
+// inline non member operator overloads - must be defined in the .h file
+inline Position operator - (const Position& rhs)
+{
+  return Position(-1.0 * rhs.getX(), -1.0 * rhs.getY(), -1.0 * rhs.getZ());
+}
+
+inline Position operator + (const Position& lhs, const Position& rhs)
+{
+  return Position(lhs.getX() + rhs.getX(),
+                  lhs.getY() + rhs.getY(),
+                  lhs.getZ() + rhs.getZ());
+}
+
+inline Position operator - (const Position& lhs, const Position& rhs)
+{
+  return lhs + (-rhs);
+}
+
+inline double operator * (const Position& lhs, const Position& rhs)
+{
+  return (lhs.getX() * rhs.getX() + lhs.getY() * rhs.getY() + lhs.getZ() * rhs.getZ());
+}
+
+inline Position operator * (const Position& lhs, const double& rhs)
+{
+  return Position(lhs.getX() * rhs, lhs.getY() * rhs, lhs.getZ() * rhs);
+}
+
+inline Position operator * (const Position& lhs, const int& rhs)
+{
+  return Position(lhs.getX() * rhs, lhs.getY() * rhs, lhs.getZ() * rhs);
+}
+
 #endif // POSITION_H
