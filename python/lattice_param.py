865a77c7288114787c2f12c77ab32a141a277fff
Jarin French <jarin.french@live.com>
2019-12-05 11:43:54 -0500

    Changed uo2_lattice_param.py to be more general.

---
+++ python/lattice_param.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python3
+
+from sys import argv, exit
+import os
+import argparse
+
+def check_range(val):
+    try:
+        value = float(val)
+    except ValueError as err:
+        raise argparse.ArgumentTypeError(str(err))
+
+    if value < 0 or value > 3300:
+        message = "Temperature out of fitted range. Expected 0 <= T <= 3300, got value = {}".format(value)
+        raise argparse.ArgumentTypeError(message)
+
+    return value
+
+def printFits(fits):
+    print("There are {l} fits:".format(l = len(fits)))
+    for i,fit in enumerate(fits):
+        print("  {} - {}".format(i + 1, fits[i][0]))
+
+parser = argparse.ArgumentParser(usage = '%(prog)s temperature potential', description = "Get the lattice parameter based on T given the lattice parameter database file")
+parser.add_argument('T', type = float, help = "Desired temperature")
+parser.add_argument('p', type = int, help = "The desired potential to use")
+parser.add_argument('--database', help = "Database file location", default = os.environ['HOME']+'/projects/scripts/lattice_params.db')
+
+args = parser.parse_args()
+
+fits = []
+
+with open(args.database) as f:
+    for _ in range(6):
+        next(f)
+    for line in f:
+        fits.append(line.split())
+
+if args.p > len(fits) or args.p - 1 < 0:
+    print("Invalid potential specifier.")
+    printFits(fits)
+    exit(2)
+
+
+if args.T >= float(fits[args.p - 1][1]) and args.T <= float(fits[args.p - 1][2]):
+    A = float(fits[args.p - 1][3]);
+    B = float(fits[args.p - 1][4]);
+    C = 0.0;
+elif args.T > float(fits[args.p - 1][2]) and args.T <= float(fits[args.p - 1][5]):
+    A = float(fits[args.p - 1][6]);
+    B = float(fits[args.p - 1][7]);
+    C = float(fits[args.p - 1][8]);
+else:
+    print("Error! Temperature not in fitted range.")
+
+print(u"The lattice parameter using the {} potential at {:.1f} K is {:.5f} Angstroms".format(fits[args.p - 1][0], args.T, A + B * args.T + C * args.T * args.T))
